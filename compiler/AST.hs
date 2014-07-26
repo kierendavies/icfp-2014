ab0255ade98edfbb06578de0d81fc5aa5bfcf8f5
Tslil Clingman <hiato1@gmail.com>
2014-07-26 16:33:51 +0200

    Tweaks

--- compiler/AST.hs
+++ compiler/AST.hs
@@ -1,16 +1,46 @@
-
-module AST
-( Expression(..)
-) where
+module AST (
+            Expression(..),
+            Instruction (..)
+           ) where
 
 import Data.List
 
-data Expression = ListExp [Expression] | QListExp [Expression] | IntExp Int | VarExp String | Closure [String] Expression
+data Expression = ListExp [Expression]
+                | QListExp [Expression]
+                | IntExp Int
+                | VarExp String
+                | Closure [String] Expression
+
+data Instruction = LDC Int
+                 | LD Int Int
+                 | ADD
+                 | SUB
+                 | MUL
+                 | DIV
+                 | CEQ
+                 | CGT
+                 | CGTE
+                 | ATOM
+                 | CONS
+                 | CAR
+                 | CDR
+                 | SEL Int Int
+                 | JOIN
+                 | LDF Int
+                 | AP Int
+                 | RTN
+                 | DUM Int
+                 | RAP Int
+                 | STOP
+                 | TSEL Int Int
+                 | TAP Int
+                 | TRAP Int
+                 | ST Int Int
+                      deriving (Show,Eq,Ord)
 
 instance Show Expression where
   show (ListExp xs) = "(" ++ (intercalate " " . map show $ xs) ++ ")"
   show (QListExp xs) = "'(" ++ (intercalate " " . map show $ xs) ++ ")"
   show (IntExp n) = "#" ++ show n
   show (VarExp x) = "$" ++ x
-  show (Closure args exp) = "(lambda (" ++ intercalate " " args ++ ") (" ++ show exp ++ ")"
-
+  show (Closure args exp) = "(λ (" ++ intercalate " " args ++ ") (" ++ show exp ++ ")"
