cfef6001c9d61f42b4e33b4b806a88d09805db17
Graham Manuell <grmanuell+bitbucket@gmail.com>
2014-07-26 15:30:03 +0200

    Added quoting of lists to the parser

--- compiler/AST.hs
+++ compiler/AST.hs
@@ -5,10 +5,11 @@ module AST
 
 import Data.List
 
-data Expression = ListExp [Expression] | IntExp Int | VarExp String | Closure [String] Expression
+data Expression = ListExp [Expression] | QListExp [Expression] | IntExp Int | VarExp String | Closure [String] Expression
 
 instance Show Expression where
   show (ListExp xs) = "(" ++ (intercalate " " . map show $ xs) ++ ")"
+  show (QListExp xs) = "'(" ++ (intercalate " " . map show $ xs) ++ ")"
   show (IntExp n) = "#" ++ show n
   show (VarExp x) = "$" ++ x
   show (Closure args exp) = "(lambda (" ++ intercalate " " args ++ ") (" ++ show exp ++ ")"
--- compiler/parser.hs
+++ compiler/parser.hs
@@ -8,7 +8,7 @@ program :: Parser Expression
 program = spaces *> (fmap (ListExp . ((VarExp "do") :)) (expression `sepEndBy` spaces)) <* eof
 
 expression :: Parser Expression
-expression = list <|> number <|> variable
+expression = list <|> qlist <|> number <|> variable
 
 numericLit :: Parser Int
 numericLit = (fmap (read) $ many1 (digit <?> "")) <?> "number"
@@ -16,7 +16,13 @@ numericLit = (fmap (read) $ many1 (digit <?> "")) <?> "number"
 number = fmap IntExp numericLit
 
 list :: Parser Expression
-list = between (char '(' *> spaces) (spaces <* char ')') (fmap ListExp $ expression `sepEndBy` (many1 space)) <?> "list"
+list = fmap ListExp listLit
+
+qlist :: Parser Expression
+qlist = char '\'' *> fmap QListExp listLit
+
+listLit :: Parser [Expression]
+listLit = between (char '(' *> spaces) (spaces <* char ')') (expression `sepEndBy` (many1 space)) <?> "list"
 
 variable :: Parser Expression
 variable = fmap VarExp (liftA2 (:) letter (many alphaNum)) <?> "variable"
